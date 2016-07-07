f2997a202be16f850f86bdeb2f42f362913f1db2
Akihiro Suda <suda.akihiro@lab.ntt.co.jp>
2016-07-07 12:20:03 +0000

    support pure bsh

--- test.sh
+++ test.sh
@@ -1,28 +1,28 @@
-#!/bin/bash
+#!/bin/sh
 set -e #exit on an error
 
 : ${MOUNT_POINT:=/tmp/mnt24309}
 
-function ERROR(){
-    echo -e "\e[101m\e[97m[ERROR]\e[49m\e[39m $@"
+ERROR(){
+    echo [ERROR] $@
 }
 
 
-function INFO(){
-    echo -e "\e[104m\e[97m[INFO]\e[49m\e[39m $@"
+INFO(){
+    echo [INFO] $@
 }
 
-function do_test() {
+do_test() {
     case=$1
     set -x
     mount -t aufs -o br:$case/rev2=ro+wh:$case/rev1=ro+wh:$case/rev0=ro+wh none ${MOUNT_POINT}
-    [ -f ${MOUNT_POINT}/x/a ] && ( ERROR "TEST FAIL: /x/a should not exist" ; exit 1)
+    [ -f ${MOUNT_POINT}/x/a ] && { ERROR "TEST FAIL: /x/a should not exist" ; exit 1; }
     umount ${MOUNT_POINT}
     set +x
     INFO "PASS: $case\n"
 }
 
-[ ${EUID:-${UID}} = 0 ] || ( ERROR "run as root"; exit 1 )
+[ $(id -u) = 0 ] || { ERROR "run as root"; exit 1; }
 
 INFO "Using mount point ${MOUNT_POINT}"
 [ -d ${MOUNT_POINT} ] || ( INFO "Creating ${MOUNT_POINT}"; mkdir -p ${MOUNT_POINT} )
@@ -43,3 +43,4 @@ do_test case01
 
 INFO "Running case02 (reported in https://github.com/docker/docker/issues/24309 )"
 do_test case02
+
